ad86a2215be71b6e5208438cf7a5fd439202b0ba
Josh Ellithorpe <quest@mac.com>
2016-10-01 16:51:23 -0700

    Run plugins concurrently

--- cmd/files.go
+++ cmd/files.go
@@ -1,11 +1,13 @@
 package cmd
 
 import (
+	"bytes"
 	"fmt"
 	"io/ioutil"
 	"os"
 	"runtime"
 	"strings"
+	"sync"
 
 	"github.com/asaskevich/govalidator"
 	"github.com/spf13/cobra"
@@ -123,37 +125,81 @@ func filesCommand(cmd *cobra.Command, args []string) error {
 }
 
 func runPlugins(pluginConfig *plugins.PluginConfig) error {
+	wg := &sync.WaitGroup{}
+
+	outputChan := make(chan string)
+	defer close(outputChan)
+
+	finishedChan := make(chan bool)
+	defer close(finishedChan)
+
+	wg.Add(1)
+	go processPluginOutput(wg, outputChan, finishedChan)
+
 	for _, name := range plugins.PluginNames() {
-		err := plugins.Plugins[name].Setup()
-		if err != nil {
-			fmt.Printf("[Warn] Failed to setup %q plugin: %s\n", name, err)
-			continue
-		}
-
-		requestID, pluginResponse, err := plugins.Plugins[name].Perform(pluginConfig)
-		if err != nil {
-			fmt.Printf("[Warn] Failed running perform on %q plugin: %s\n", name, err)
-			continue
-		}
-
-		tagMap, err := pluginResponse.Tags(requestID)
-		if err != nil {
-			return err
-		}
-
-		displayTags(name, tagMap)
+		wg.Add(1)
+		go runPlugin(name, pluginConfig, wg, outputChan)
 	}
+
+	wg.Wait()
+
+	finishedChan <- true
 
 	return nil
 }
 
-func displayTags(name string, tagMap map[string][]string) {
-	if len(tagMap) > 0 {
-		for asset, tags := range tagMap {
-			fmt.Printf("%s - %s\n", name, asset)
-			fmt.Printf("%v\n", tags)
+func processPluginOutput(wg *sync.WaitGroup, outputChan <-chan string, finishedChan <-chan bool) {
+	wg.Done()
+
+Loop:
+	for {
+		select {
+		case output := <-outputChan:
+			fmt.Printf(output)
+		case <-finishedChan:
+			break Loop
 		}
 	}
+	return
+}
+
+func runPlugin(name string, pluginConfig *plugins.PluginConfig, wg *sync.WaitGroup, outputChan chan<- string) {
+	defer wg.Done()
+
+	err := plugins.Plugins[name].Setup()
+	if err != nil {
+		outputChan <- fmt.Sprintf("[Warn] Failed to setup %q plugin: %s\n", name, err)
+		return
+	}
+
+	requestID, pluginResponse, err := plugins.Plugins[name].Perform(pluginConfig)
+	if err != nil {
+		outputChan <- fmt.Sprintf("[Warn] Failed running perform on %q plugin: %s\n", name, err)
+		return
+	}
+
+	tagMap, err := pluginResponse.Tags(requestID)
+	if err != nil {
+		return
+	}
+
+	outputChan <- displayTags(name, tagMap)
+
+	return
+}
+
+func displayTags(name string, tagMap map[string][]string) string {
+	var buf []byte
+	output := bytes.NewBuffer(buf)
+
+	if len(tagMap) > 0 {
+		for asset, tags := range tagMap {
+			output.WriteString(fmt.Sprintf("%s - %s\n", name, asset))
+			output.WriteString(fmt.Sprintf("%v\n", tags))
+		}
+	}
+
+	return output.String()
 }
 
 func sortItems(items []string) (urls []string, files []*os.File, errs []error) {
