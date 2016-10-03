04d1226acc8d7d0eda9fc5f818b364bf8e52a136
Michael Jackson <michaelpauljackson@gmail.com>
2016-10-03 16:42:51 -0700

    Added file upload support for imagga.

--- visagoapi/imagga/types.go
+++ visagoapi/imagga/types.go
@@ -13,3 +13,13 @@ type resultEntryTag struct {
 	Confidence float64 `json:"confidence"`
 	Tag        string  `json:"tag"`
 }
+
+type resultFile struct {
+	Status		string 					`json:"status"`
+	Uploaded 	[]*resultUpload `json:"uploaded"`
+}
+
+type resultUpload struct {
+	ID 				string `json:"id"`
+	Filename 	string `json:"filename"`
+}
